082704fc378ee2cb6c253e0a974a7a612030d29a
JuniorCai <JuniorCai@outlook.com>
2018-10-31 16:00:55 +0800

    完善登录逻辑

--- pages/account/login/login.js
+++ pages/account/login/login.js
@@ -5,7 +5,12 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+      disabled:true,
+      loginModel:{
+        phone:"",
+        password:"",
+        loginType:1,
+      }
   },
 
   /**
@@ -62,5 +67,67 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+  phoneInput:function(e){
+      this.setData({
+        "loginModel.phone":e.detail.value,
+      });
+  },
+  passwordInput: function (e) {
+    this.setData({
+      "loginModel.password": e.detail.value,
+    });
+  },
+  onLogin:function(e){
+    if (this.data.loginModel.phone.length == 0 || this.data.loginModel.password.length == 0)    {
+      wx.showToast({
+        title: '手机号码或密码不能为空',
+        mask:true,
+        duration:1500
+      });
+      setTimeout(function(){
+        wx.hideToast()
+      },2000);
+    } else if (this.data.loginModel.phone.length != 11){
+      wx.showToast({
+        title: '请输入11位手机号码!',
+        icon: 'none',
+        duration: 1500
+      })
+
+      setTimeout(function () {
+        wx.hideToast()
+      }, 2000)
+    } else if (e.detail.value.password.length < 6 || e.detail.value.password.length > 20) {
+      wx.showToast({
+        title: '请输入6-20密码!',
+        icon: 'none',
+        duration: 1500
+      })
+      setTimeout(function () {
+        wx.hideToast()
+      }, 2000)
+    }else{
+      wx.request({
+        url: 'http://localhost:6234/api/Account/Authenticate',
+        data: this.data.loginModel,
+          method: 'POST',
+          header: { 'content-type': 'application/json' },
+          success: function (data) {
+          console.info(data);
+        }
+      })
+    }
+  },
+  onCheckInput:function(e){
+    if (this.data.loginModel.phone.length == 0 || this.data.loginModel.password.length==0){
+      this.setData({
+        disabled:true
+      })
+    }else{
+      this.setData({
+        disabled: false
+      })
+    }
   }
 })
