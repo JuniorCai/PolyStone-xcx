08256fdb3c16dbdc622d12ad458e117336f6ef50
JuniorCai <JuniorCai@outlook.com>
2018-11-14 15:41:35 +0800

    添加修改头像相关页面及功能

--- pages/center/userInfo/userInfo.js
+++ pages/center/userInfo/userInfo.js
@@ -1,3 +1,5 @@
+import Toast from "../../../customComponent/VantWeapp/toast/toast";
+
 const app = getApp()
 
 // pages/center/UserInfo/userInfo.js
@@ -8,18 +10,22 @@ Page({
    */
   data: {
     hasUserInfo: false,
-    userInfo: {}
+    userInfo: {},
+    phoneNumberMix:""
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+
     if (!this.data.hasUserInfo && app.globalData.hasUserInfo) {
       this.setData({
         hasUserInfo: app.globalData.hasUserInfo,
-        userInfo: app.globalData.userInfo
-      })
+        userInfo: app.globalData.userInfo,
+        phoneNumberMix: app.globalData.hasUserInfo.phoneNumber.substr(0, 3) + "****" + app.globalData.hasUserInfo.phoneNumber.substr(8, 4)
+      });
+
     }else{
       // wx.redirectTo({
       //   url: '../../account/login/login',
@@ -73,6 +79,40 @@ Page({
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
+
+  },
+  chooseAvatar:function(){
+    var that = this; 
+    wx.chooseImage({
+      count: 1, 
+      success: function (res) { // 无论用户是从相册选择还是直接用相机拍摄，路径都是在这里面 
+
+        var filePath = res.tempFilePaths[0]; //将刚才选的照片/拍的 放到下面view视图中 
+        that.setData({ 
+          "userInfo.avatar": filePath, //把照片路径存到变量中， 
+        }); 
+
+      // 这个是使用微信接口保存文件到数据库 
+      // wx.uploadFile({ 
+        // url: "", 
+        // filePath: filePath, 
+        // name: 'file',
+        // success: function (res) { 
+          // } 
+          // }) 
+          }, fail: function (error) { 
+                console.error("调用本地相册文件时出错");
+                console.warn(error) 
+             },
+      complete: function () {
+        Toast.loading({
+          duration: 5000,
+          mask: true,
+          message: "测试"
+        })
+
+        }
+    });
 
   }
 })
