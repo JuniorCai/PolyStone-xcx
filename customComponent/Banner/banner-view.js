3e689bfd23808710e68f59f6b3723dbaf649daab
JuniorCai <JuniorCai@outlook.com>
2019-03-05 21:12:52 +0800

    添加轮播组件
    待处理：处理轮播组件样式及脚本接口逻辑

---
+++ customComponent/Banner/banner-view.js
@@ -0,0 +1,34 @@
+// customComponent/Banner/banner-view.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    interval:{
+      type:Number,
+      value:5000
+    },
+    duration:{
+      type:Number,
+      value:500
+    },
+    bannerList:{
+      type:Array,
+      value:[]
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
